55960b70e7267ef5d884bb05ceb7c7490d228176
sansayin <1818237@qq.com>
2023-03-10 10:48:27 -0800

    clean up code

--- src/components/ChatRow.tsx
+++ src/components/ChatRow.tsx
@@ -33,13 +33,11 @@ function ChatRow({ id }: Props) {
 
     return (
         <Link href={`/chat/${id}`} className={`chatRow justify-center`}>
-            <div className="flex flex-row">
-            <ChatBubbleLeftIcon className="flex-1 h-5 w-5" />
-            <p className="flex=2 hidden md:inline-flex truncate">
+            <ChatBubbleLeftIcon className="h-5 w-5" />
+            <p className="flex=1 hidden md:inline-flex truncate">
                 {messages?.docs[messages.docs.length-1]?.data().text || "New Chat"}
             </p>
-            <TrashIcon onClick={removeChat} className="flex-3 h-5 w-5 text-gray-700 hover:text-red-700" />
-            </div>
+            <TrashIcon onClick={removeChat} className="h-5 w-5 text-gray-700 hover:text-red-700" />
         </Link>
     )
 }
